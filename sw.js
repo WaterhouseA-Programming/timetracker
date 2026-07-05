90388bedc0e7f476992236386b8e8c8193816237
WaterhouseA-Programming <WaterhouseA-Programming@users.noreply.github.com>
2026-07-05 16:50:40 +0000

    deploy: bfcf88726ffb6d0c58b69580984d1236d0a511cd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tt-tasks-v20';
+const CACHE = 'tt-tasks-v21';
 // Use self.location so paths work whether served from / or a subpath (e.g. GitHub Pages)
 const BASE  = self.location.pathname.replace(/sw\.js$/, '');
 const SHELL = [
